f6b41326fec186c68c51bac60be2d59f9f3e332e
Viktor Čierňava <kontakt@viktorciernava.eu>
2023-05-27 01:49:43 +0200

    New: Dynamic Board, now we can have as much board as we want.

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -3,8 +3,13 @@ import React, {useState} from "react";
 import {DragDropContext, Draggable, Droppable} from "react-beautiful-dnd";
 
 export default function Welcome() {
-    const TemplateTasks = {
-        Completed: [
+    const BoardTitles = [
+        "Completed Tasks",
+        "New Tasks",
+        "On hold Tasks"
+    ]
+    const TemplateBoard = [
+        [
             {
                 id: "1",
                 title: "First Task",
@@ -19,18 +24,25 @@ export default function Welcome() {
                 id: "3",
                 title: "Third Task",
                 description: "amet est placerat in egestas erat imperdiet sed euismod nisi porta lorem mollis aliquam ut porttitor leo a diam sollicitudin tempor id eu nisl nunc"
-            }
+            },
         ],
-        New: [
+        [
             {
                 id: "4",
                 title: "Fourth Task",
                 description: "amet est placerat in egestas erat imperdiet sed euismod nisi porta lorem mollis aliquam ut porttitor leo a diam sollicitudin tempor id eu nisl nunc"
-            }
-        ]
-    }
+            },
+        ],
+        [
+            {
+                id: "5",
+                title: "Fifth Task",
+                description: "amet est placerat in egestas erat imperdiet sed euismod nisi porta lorem mollis aliquam ut porttitor leo a diam sollicitudin tempor id eu nisl nunc"
+            },
+        ],
+    ]
 
-    const [Tasks, updateTasks] = useState(TemplateTasks)
+    const [Board, updateBoard] = useState(TemplateBoard)
     const reorder = (list, startIndex, endIndex) => {
         const result = Array.from(list);
         const [removed] = result.splice(startIndex, 1);
@@ -38,7 +50,6 @@ export default function Welcome() {
 
         return result;
     };
-
     const move = (source, destination, droppableSource, droppableDestination) => {
         const sourceClone = Array.from(source);
         const destClone = Array.from(destination);
@@ -52,50 +63,30 @@ export default function Welcome() {
 
         return result;
     };
-
-    const getList = id => Tasks[id];
-
-    const handleCompletedTasks = result => {
+    const handleTasks = result => {
         const { source, destination } = result;
 
         // dropped outside the list
         if (!destination) {
             return;
         }
+        const sInd = +source.droppableId;
+        const dInd = +destination.droppableId;
 
-        if (source.droppableId === destination.droppableId) {
-            const items = reorder(
-                getList(source.droppableId),
-                source.index,
-                destination.index
-            );
-
-            if (source.droppableId === 'New') {
-                updateTasks({
-                    Completed: Tasks.Completed,
-                    New: items
-                })
-            } else {
-                updateTasks({
-                    Completed: items,
-                    New: Tasks.New
-                })
-            }
-
+        if (sInd === dInd) {
+            const items = reorder(Board[sInd], source.index, destination.index);
+            const newState = [...Board];
+            newState[sInd] = items;
+            updateBoard(newState);
         } else {
-            const result = move(
-                getList(source.droppableId),
-                getList(destination.droppableId),
-                source,
-                destination
-            );
+            const result = move(Board[sInd], Board[dInd], source, destination);
+            const newState = [...Board];
+            newState[sInd] = result[sInd];
+            newState[dInd] = result[dInd];
 
-            updateTasks({
-                Completed: result.Completed,
-                New: result.New
-            });
+            updateBoard(newState.filter(group => group.length));
         }
-    };
+    }
 
     const getListStyle = isDraggingOver => {
         return isDraggingOver ? "bg-amber-50" : "bg-slate-50"
@@ -108,58 +99,37 @@ export default function Welcome() {
     return (
         <>
             <Head title="Todos" />
-            <section className="flex items-start justify-center gap-6 bg-gray-50 pt-40 h-screen w-screen">
-                    <DragDropContext onDragEnd={handleCompletedTasks}>
-                        <div className="w-1/3 min-h-[40rem] max-h-fit">
-                            <h1 className="text-4xl py-4">Completed Tasks</h1>
-                            <Droppable droppableId="Completed">
-                                {(provided, snapshot) => (
-                                    <ul className={`${getListStyle(snapshot.isDraggingOver)} border border-gray-100 rounded-lg px-6 py-4 min-h-[40rem] transition-all`} {...provided.droppableProps} ref={provided.innerRef}>
-                                        {Tasks.Completed.map(({id, title, description}, index) => {
-                                            return (
-                                                <Draggable key={id} draggableId={id} index={index}>
-                                                    {(provided, snapshot) => (
-                                                        <li className={`${getItemStyle(snapshot.isDragging)} relative bg-white rounded-sm px-6 py-4 my-2 border border-gray-200 transition-border-rd ${index === 0 ? "rounded-t-lg" : ""} ${index === Tasks.Completed.length-1 ? "rounded-b-lg" : ""}`} ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
-                                                            <h2 className="text-lg font-bold text-black">{title}</h2>
-                                                            <p className="text-slate-600">
-                                                                { description }
-                                                            </p>
-                                                            <span className="absolute bottom-2 right-2 text-xs text-slate-600">id:{id}</span>
-                                                        </li>
-                                                    )}
-                                                </Draggable>
-                                            );
-                                        })}
-                                        {provided.placeholder}
-                                    </ul>
-                                )}
-                            </Droppable>
-                        </div>
-                        <div className="w-1/3 min-h-[40rem] max-h-fit">
-                            <h1 className="text-4xl py-4">New Tasks</h1>
-                            <Droppable droppableId="New">
-                                {(provided, snapshot) => (
-                                    <ul className={`${getListStyle(snapshot.isDraggingOver)} border border-gray-100 rounded-lg px-6 py-4 min-h-[40rem] transition-all`} {...provided.droppableProps} ref={provided.innerRef}>
-                                        {Tasks.New.map(({id, title, description}, index) => {
-                                            return (
-                                                <Draggable key={id} draggableId={id} index={index}>
-                                                    {(provided, snapshot) => (
-                                                        <li className={`${getItemStyle(snapshot.isDragging)} relative bg-white rounded-sm px-6 py-4 my-2 border border-gray-200 transition-border-rd ${index === 0 ? "rounded-t-lg" : ""} ${index === Tasks.New.length-1 ? "rounded-b-lg" : ""}`} ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
-                                                            <h2 className="text-lg font-bold text-black">{title}</h2>
-                                                            <p className="text-slate-600">
-                                                                { description }
-                                                            </p>
-                                                            <span className="absolute bottom-2 right-2 text-xs text-slate-600">id:{id}</span>
-                                                        </li>
-                                                    )}
-                                                </Draggable>
-                                            );
-                                        })}
-                                        {provided.placeholder}
-                                    </ul>
-                                )}
-                            </Droppable>
-                        </div>
+            <section className="flex items-start justify-center gap-6 bg-gray-50 pt-40 px-10 h-screen w-screen">
+                    <DragDropContext onDragEnd={handleTasks}>
+                        {
+                            Board.map((board, ind) => (
+                                <div className="w-1/5 min-h-[40rem] max-h-fit">
+                                    <h1 className="text-lg text-black font-bold mb-4">{BoardTitles[ind]}</h1>
+                                    <Droppable key={ind} droppableId={`${ind}`}>
+                                        {(provided, snapshot) => (
+                                            <ul className={`${getListStyle(snapshot.isDraggingOver)} border border-gray-100 rounded-lg px-6 py-4 min-h-[40rem] transition-all`} {...provided.droppableProps} ref={provided.innerRef}>
+                                                {board.map(({id, title, description}, index) => {
+                                                    return (
+                                                        <Draggable key={id} draggableId={id} index={index}>
+                                                            {(provided, snapshot) => (
+                                                                <li className={`${getItemStyle(snapshot.isDragging)} relative bg-white rounded-sm px-6 py-4 my-2 border border-gray-200 transition-border-rd ${index === 0 ? "rounded-t-lg" : ""} ${index === board.length-1 ? "rounded-b-lg" : ""}`} ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                                                                    <h2 className="text-lg font-bold text-black">{title}</h2>
+                                                                    <p className="text-slate-600">
+                                                                        { description }
+                                                                    </p>
+                                                                    <span className="absolute bottom-2 right-2 text-xs text-slate-600">id:{id}</span>
+                                                                </li>
+                                                            )}
+                                                        </Draggable>
+                                                    );
+                                                })}
+                                                {provided.placeholder}
+                                            </ul>
+                                        )}
+                                    </Droppable>
+                                </div>
+                            ))
+                        }
                     </DragDropContext>
             </section>
         </>
